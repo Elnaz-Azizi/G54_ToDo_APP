b0075f31b89cf3e106bd72eab1667f9157d7c58f
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-04-23 09:52:08 +0200

    Introduce Builder annotation, create entities (Person & Todo) and add spring profile

--- src/main/java/se/lexicon/todo_app/entity/Person.java
+++ src/main/java/se/lexicon/todo_app/entity/Person.java
@@ -1,5 +1,23 @@
 package se.lexicon.todo_app.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+//@Builder // Builder Design Pattern
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "persons")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
 
 }
--- src/main/java/se/lexicon/todo_app/entity/Todo.java
+++ src/main/java/se/lexicon/todo_app/entity/Todo.java
@@ -1,6 +1,54 @@
 package se.lexicon.todo_app.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//@Builder
+
+@Entity
+@Table(name = "todos")
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+    private LocalDateTime dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
 
 }
